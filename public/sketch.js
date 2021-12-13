9deca13930497689a840824415d4555ce9bc607e
Lorenzo Bernini <lorenzobernini98@gmail.com>
2021-12-14 00:33:27 +0100

    mona lisa

--- public/sketch.js
+++ public/sketch.js
@@ -1,36 +1,54 @@
-let clientSocket;
+let clientSocket = io();
 
 clientSocket.on("connect", newConnection);
 clientSocket.on("mouseBroadcast", newBroadcast);
-
-function preload() {
-  clientSocket = io();
-}
 
 function newConnection() {
   console.log("Your id: ", clientSocket.id);
 }
 
+// esegui questa funzione con i dati ricevuti
 function newBroadcast(data) {
   console.log(data);
-  fill("red");
-  circle(data.x, data.y, 10);
+  // fill("red");
+  // circle(data.x, data.y, 10);
+  stroke("red");
+  strokeWeight(8);
+  line(data.x, data.y, data.px, data.py);
+}
+
+let gioconda;
+
+function preload() {
+  gioconda = loadImage("assets/gioconda.jpg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220);
+  background(0);
+  textSize(37);
+  image(gioconda, 0, 0);
+  fill(255);
+  text("Let's vandalize the Mona Lisa toghether!", 10, 40);
 }
 
 function draw() {
-  fill("yellow");
-  circle(mouseX, mouseY, 40);
+  stroke(0);
+  strokeWeight(8);
+
+  //disegna tenendo premuto
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
 
-function mouseMoved() {
+//la funzione mouseDragged contiene la posizione del mouse quando è premuto il click e viene usata come messaggio da mandare al server
+function mouseDragged() {
   let message = {
     x: mouseX,
     y: mouseY,
+    px: pmouseX,
+    py: pmouseY,
   };
 
   clientSocket.emit("mouse", message);
